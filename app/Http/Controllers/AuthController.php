3b1c893c84909c170191fc563097754d43037611
Qabas-mamari <qabasalmamari@gmail.com>
2022-08-12 00:38:39 +0400

    Login on Backend

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Password;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 
 class AuthController extends Controller
@@ -42,6 +43,36 @@ class AuthController extends Controller
         return response([
             'user' => $user,
             'token'=> $token
+        ], 200);
+    }
+
+    // create login function 
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email|string|exists:users,email',
+            'password' => [
+                'required',
+            ],
+            'remember' => 'boolean' // "remember me" checkbox 
+        ]);
+
+        $remember = $credentials['remember'] ?? false; // false by default 
+        unset($credentials['remember']);
+        
+        //The attempt method will return true if authentication was successful. Otherwise, false will be returned.
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'error' => 'The Provided credentials are not correct'
+            ], 422);
+        }
+        // give the user authorize to User model
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
         ], 200);
     }
 }
